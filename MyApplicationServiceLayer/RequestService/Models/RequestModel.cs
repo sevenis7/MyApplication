bb5b3988d1b9d952ff87bb498bb81014c1cf3188
Евгений Горбунов <gorbunovea7@gmail.com>
2024-04-01 19:10:49 +0300

    Email is not required in RequestModel, cuz in User class is not required too

--- MyApplicationServiceLayer/RequestService/Models/RequestModel.cs
+++ MyApplicationServiceLayer/RequestService/Models/RequestModel.cs
@@ -7,6 +7,6 @@
         public required string Text { get; set; }
         public required string UserName { get; set; }
         public required string Status { get; set; }
-        public required string Email { get; set; }
+        public string? Email { get; set; }
     }
 }
