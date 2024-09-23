5decb5e9344ce8b8dc45740782130bfe2d3589c4
Евгений Горбунов <gorbunovea7@gmail.com>
2024-09-23 23:42:30 +0300

    ProjectModel Changed

--- MyApplicationDataLayer/DataContext/AppDbContext.cs
+++ MyApplicationDataLayer/DataContext/AppDbContext.cs
@@ -16,7 +16,7 @@ namespace MyApplicationDataLayer.DataContext
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
+            Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
---
+++ MyApplicationDataLayer/Migrations/20240923204035_ProjectModelChanged.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApplicationDataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectModelChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Projects");
+
+            migrationBuilder.RenameColumn(
+                name: "Text",
+                table: "Projects",
+                newName: "ImageUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Components",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Projects");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Projects",
+                newName: "Text");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Components",
+                newName: "id");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "Projects",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+    }
+}
